636206e613dbb2ad3dafe738197ff0f0ff683f77
Valentin Dujour <valentin.dujour@icloud.com>
2026-09-10 15:18:29 +0200

    Exercice 7 réalisé !

--- src/net/lecnam/ussi47/tp2/Exercice7.java
+++ src/net/lecnam/ussi47/tp2/Exercice7.java
@@ -1,9 +1,19 @@
 package net.lecnam.ussi47.tp2;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Exercice7 {
 
     public static void main(String[] args) {
+        int nombrePairOuImpair = ThreadLocalRandom.current().nextInt(1,101);
 
+        if (nombrePairOuImpair % 2 == 0 ) {
+            /*Si le chiffre est pair*/
+            System.out.println("Le nombre " + nombrePairOuImpair + " est pair");
+        } else {
+            /*Si le chiffre est impair*/
+            System.out.println("Le nombre " + nombrePairOuImpair + " est impair");
+        }
 
     }
 }
