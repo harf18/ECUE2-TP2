f2f12d4eb73b8356dd245232720eb666fb4735eb
Xavier Barbeau <xavierbarbeau41@gmail.com>
2026-09-10 12:05:27 +0200

    Exercice 4

--- src/net/lecnam/ussi47/tp2/Exercice4.java
+++ src/net/lecnam/ussi47/tp2/Exercice4.java
@@ -3,7 +3,9 @@ package net.lecnam.ussi47.tp2;
 public class Exercice4 {
 
     public static void main(String[] args) {
-
-
+        int jumpHeightMeter = 4000;
+        float keyConvertionMetersFoot = 3.28084f;
+        float jumpHeightFoot = jumpHeightMeter * keyConvertionMetersFoot;
+        System.out.println("Pour convertire une longueur en metre en pieds, il faut mutliplier la longueur par " + keyConvertionMetersFoot + ".\nAinsi, un saut de " + jumpHeightMeter + "m est de : " + jumpHeightFoot + "ft.") ;
     }
 }
