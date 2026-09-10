60941923abe5a74ffd2d48e516802dd89be59b1f
Julien Forestier <julien.forestier.auditeur@lecnam.net>
2026-09-10 11:10:39 +0200

    Exercice1

--- src/net/lecnam/ussi47/tp2/Exercice1.java
+++ src/net/lecnam/ussi47/tp2/Exercice1.java
@@ -3,6 +3,24 @@ package net.lecnam.ussi47.tp2;
 public class Exercice1 {
 
     public static void main(String[] args) {
+        byte i = 12;
+        float j = 2.26f;
+        int k = -300;
+        long l = 3000000000L;
+        long m = 2000000000;
+        String w = "a";
+        String x = "Hello";
+        byte c = 1;
+        int v = 29000;
+
+
+
+
+
+
+
+
+
 
 
     }
