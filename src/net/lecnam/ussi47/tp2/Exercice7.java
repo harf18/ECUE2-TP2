d75aea97ebc4e8219e89e210bf0f1975ba7b60c8
Andrea Andreoli <andrea.andreoli.auditeur@lecnam.net>
2026-09-10 12:23:57 +0200

    Exercice 7

--- src/net/lecnam/ussi47/tp2/Exercice7.java
+++ src/net/lecnam/ussi47/tp2/Exercice7.java
@@ -1,9 +1,15 @@
 package net.lecnam.ussi47.tp2;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Exercice7 {
 
     public static void main(String[] args) {
-
-
+        int nombreAleatoire = ThreadLocalRandom.current().nextInt(1,101);
+        if (nombreAleatoire % 2 == 0){
+            System.out.println("Le nombre aléatoire " + nombreAleatoire + " est pair");
+        } else {
+            System.out.println("Le nombre aléatoire " + nombreAleatoire + " est impair");
+        }
     }
 }
