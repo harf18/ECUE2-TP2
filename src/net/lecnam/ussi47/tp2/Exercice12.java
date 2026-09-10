4565e4c583f10a527f75117c8772ed3ed97deb38
Andrea Andreoli <andrea.andreoli.auditeur@lecnam.net>
2026-09-10 15:34:48 +0200

    Exercice 12

--- src/net/lecnam/ussi47/tp2/Exercice12.java
+++ src/net/lecnam/ussi47/tp2/Exercice12.java
@@ -1,9 +1,21 @@
 package net.lecnam.ussi47.tp2;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Exercice12 {
 
     public static void main(String[] args) {
+        List<String> voyelles = Arrays.asList("a", "e", "i", "o", "u", "y");
+        String phraseAComparer = "cette phrase contient 11 voyelles";
+        int compteur = 0;
 
-
+        for (int i = 0 ; i < phraseAComparer.length() ;i++ ){
+            String lettre = String.valueOf(phraseAComparer.charAt(i));
+            if (voyelles.contains(lettre)){
+                compteur++;
+            }
+        }
+        System.out.println("le nombre de voyelle dans la phrase est : " + compteur);
     }
 }
