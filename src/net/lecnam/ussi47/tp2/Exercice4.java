442f1d0078ea672af00bdec737a98c9f00dbe86b
Alioune Diop <14diopalioune2003@gmail.com>
2026-09-10 14:14:55 +0200

    Exercice 4

--- src/net/lecnam/ussi47/tp2/Exercice4.java
+++ src/net/lecnam/ussi47/tp2/Exercice4.java
@@ -3,6 +3,12 @@ package net.lecnam.ussi47.tp2;
 public class Exercice4 {
 
     public static void main(String[] args) {
+        float pieds = 3.28f;
+        float nombreDePieds;
+        int distance = 4000 ;
+        nombreDePieds = pieds * distance;
+        System.out.println("un metre correspond à "+ pieds + " pieds");
+        System.out.println(" Le nombre de pieds sur une distance de "+ distance+ " métres est de " +nombreDePieds);
 
 
     }
