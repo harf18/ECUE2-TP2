2f2bf4fdc37907507dbc4232026df7944f63726e
Stephane Bance <bancestephane.r@gmail.com>
2026-09-10 07:28:21 -0200

    Exercice 2 Fini

--- src/net/lecnam/ussi47/tp2/Exercice2.java
+++ src/net/lecnam/ussi47/tp2/Exercice2.java
@@ -1,9 +1,17 @@
 package net.lecnam.ussi47.tp2;
 
+import static java.lang.Math.pow;
+
 public class Exercice2 {
 
     public static void main(String[] args) {
+        int c_adjacent = 3;
+        int c_oppose = 4;
+        int hypotenuse = 5;
 
+        boolean test_pytagore = pow(c_adjacent, 2) + pow(c_oppose, 2) == pow(hypotenuse, 2);
+
+        if (test_pytagore) System.out.println("Le triangle est rectangle.");
 
     }
 }
