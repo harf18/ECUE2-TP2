374cfd6032cc6fab703b7871b178fbd42c95b7a0
Zackarie Nouchet <zackarie.nouchet.auditeur@lecnam.net>
2026-09-10 11:04:23 +0200

    Exercice 1

--- src/net/lecnam/ussi47/tp2/Exercice1.java
+++ src/net/lecnam/ussi47/tp2/Exercice1.java
@@ -4,6 +4,15 @@ public class Exercice1 {
 
     public static void main(String[] args) {
 
+        byte a = 12;
+        float b = 2.26f;
+        short c = -300;
+        long d = 3000000000L;
+        int e = 2000000000;
+        char f = 'a';
+        String g = "Hello";
+        byte h = 1;
+        short i = 29000;
 
     }
 }
