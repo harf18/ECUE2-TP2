01f233bcba569fbdcc1c9b9fffa25a47db1d01c0
Julien Forestier <julien.forestier.auditeur@lecnam.net>
2026-09-10 11:20:32 +0200

    Exercice2

--- src/net/lecnam/ussi47/tp2/Exercice2.java
+++ src/net/lecnam/ussi47/tp2/Exercice2.java
@@ -3,7 +3,18 @@ package net.lecnam.ussi47.tp2;
 public class Exercice2 {
 
     public static void main(String[] args) {
+        int cote1 = 3;
+        int cote2 = 4;
+        int cote3 = 5;
 
+        int sommeCarres = cote1 * cote1 + cote2 * cote2;
 
+        int carreHypotenuse = cote3 * cote3;
+
+        if (sommeCarres == carreHypotenuse) {
+            System.out.println("Le triangle est rectangle");
+        } else {
+            System.out.println("Le triangle n'est pas rectangle");
+        }
     }
 }
