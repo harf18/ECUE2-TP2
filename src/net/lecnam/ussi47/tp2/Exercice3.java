781d6290f1f42ea47c82b4ca2b8c2aaced0bdcbb
Alioune Diop <14diopalioune2003@gmail.com>
2026-09-10 12:27:07 +0200

    Exercice 3

--- src/net/lecnam/ussi47/tp2/Exercice3.java
+++ src/net/lecnam/ussi47/tp2/Exercice3.java
@@ -3,7 +3,15 @@ package net.lecnam.ussi47.tp2;
 public class Exercice3 {
 
     public static void main(String[] args) {
-
-
+        float cote = 1.25f ;
+        int nombreDePlante;
+        float surfaceCult;
+        float surfaceParPlante;
+        nombreDePlante = 4*9;
+        System.out.println(nombreDePlante);
+        surfaceCult = cote*cote*4;
+        System.out.println(surfaceCult);
+        surfaceParPlante = surfaceCult/nombreDePlante;
+        System.out.println(surfaceParPlante);
     }
 }
