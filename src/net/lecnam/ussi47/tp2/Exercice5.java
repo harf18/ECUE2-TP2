bef6f8d605d84579a029bbc01cd8d972e2879320
Valentin Dujour <valentin.dujour@icloud.com>
2026-09-10 12:32:00 +0200

    Exercice 5 réalisé !

--- src/net/lecnam/ussi47/tp2/Exercice5.java
+++ src/net/lecnam/ussi47/tp2/Exercice5.java
@@ -3,7 +3,11 @@ package net.lecnam.ussi47.tp2;
 public class Exercice5 {
 
     public static void main(String[] args) {
+        double surfacePiece = 3.4 * 4.3 * 2.3;
+        System.out.println(surfacePiece);
 
+        double nombreDeRadiateurTotal = (surfacePiece * 1) / 8;
+        System.out.println("Il faut " + Math.ceil(nombreDeRadiateurTotal) + " radiateurs");
 
     }
 }
