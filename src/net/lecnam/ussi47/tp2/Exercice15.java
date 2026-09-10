8641ec822dfae26c748dd451fc779c0398648f34
Stephane Bance <bancestephane.r@gmail.com>
2026-09-10 12:01:21 -0200

    Exercice 15 Fini !

--- src/net/lecnam/ussi47/tp2/Exercice15.java
+++ src/net/lecnam/ussi47/tp2/Exercice15.java
@@ -3,6 +3,14 @@ package net.lecnam.ussi47.tp2;
 public class Exercice15 {
 
     public static void main(String[] args) {
+        int height = 15;
+        String line = "";
+
+        for (int i = 1; i <= height;i++)
+        {
+            line = " ".repeat(height - i) + "*".repeat(2*i - 1);
+            System.out.println(line);
+        }
 
 
     }
