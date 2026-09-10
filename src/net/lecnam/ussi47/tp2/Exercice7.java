82ceeb03c471a6ab789fd545e264f250ac253f51
Xavier Barbeau <xavierbarbeau41@gmail.com>
2026-09-10 12:28:23 +0200

    Exercice 7

--- src/net/lecnam/ussi47/tp2/Exercice7.java
+++ src/net/lecnam/ussi47/tp2/Exercice7.java
@@ -1,9 +1,15 @@
 package net.lecnam.ussi47.tp2;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Exercice7 {
 
     public static void main(String[] args) {
-
-
+        byte randomNumber= (byte) ThreadLocalRandom.current().nextInt(0,101);
+        if (randomNumber % 2 == 0) {
+            System.out.println("Le nombre " + randomNumber + " est pair.");
+        } else {
+            System.out.println("Le nombre " + randomNumber + " est impair.");
+        }
     }
 }
