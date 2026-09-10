b778473593c1f83aa759ab4c3421bb85381fb85f
Alioune Diop <14diopalioune2003@gmail.com>
2026-09-10 16:39:03 +0200

    Exercice 10

--- src/net/lecnam/ussi47/tp2/Exercice10.java
+++ src/net/lecnam/ussi47/tp2/Exercice10.java
@@ -3,7 +3,9 @@ package net.lecnam.ussi47.tp2;
 public class Exercice10 {
 
     public static void main(String[] args) {
-
-
+    String enMajuscule = "HELLO WORLD";
+    String enMinuscule = "hello world";
+    System.out.println(enMajuscule.toLowerCase());
+    System.out.println(enMinuscule.toUpperCase());
     }
 }
