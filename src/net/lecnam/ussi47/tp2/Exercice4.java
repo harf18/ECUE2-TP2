bbc8e135fe74a245f7f34c34cb7ffc85660377b7
Andrea Andreoli <andrea.andreoli.auditeur@lecnam.net>
2026-09-10 11:56:45 +0200

    Exercice 4

--- src/net/lecnam/ussi47/tp2/Exercice4.java
+++ src/net/lecnam/ussi47/tp2/Exercice4.java
@@ -3,7 +3,10 @@ package net.lecnam.ussi47.tp2;
 public class Exercice4 {
 
     public static void main(String[] args) {
-
+        float factureDeConversionMToP = 3.28084f ;
+        int hauteurDeSaut = 4000 ;
+        double hauteurDeSautEnPied = hauteurDeSaut * factureDeConversionMToP ;
+        System.out.println("La facture de conversion est " + factureDeConversionMToP +  " et la hauteur en \"pieds\" est de " + hauteurDeSautEnPied);
 
     }
 }
