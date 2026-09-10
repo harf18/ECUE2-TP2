c0340d3f252790f88e91bbe0fadd10d51afddabd
Mouchard Yannis <yannis.mouchard@gmail.com>
2026-09-10 13:58:05 +0200

    Exercice 15

--- src/net/lecnam/ussi47/tp2/Exercice15.java
+++ src/net/lecnam/ussi47/tp2/Exercice15.java
@@ -1,9 +1,18 @@
 package net.lecnam.ussi47.tp2;
+import java.util.Scanner;
 
 public class Exercice15 {
 
     public static void main(String[] args) {
-
-
+        int x = 5;
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < x - i - 1; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < (2 * i + 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
