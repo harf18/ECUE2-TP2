3ae9e1565e3100d30a108f9c3615dcba2d8f0d5e
Andrea Andreoli <andrea.andreoli.auditeur@lecnam.net>
2026-09-10 16:46:33 +0200

    Exercice 15

--- src/net/lecnam/ussi47/tp2/Exercice15.java
+++ src/net/lecnam/ussi47/tp2/Exercice15.java
@@ -3,7 +3,18 @@ package net.lecnam.ussi47.tp2;
 public class Exercice15 {
 
     public static void main(String[] args) {
+        int hauteur = 5;
+        String ligne = "";
+        int i = 1 ;
+        int a = 1;
 
-
+        for (i = 5 ; i > 0; i--){
+            String espace = " ";
+            String etoile = "*";
+            String nombreEspace = espace.repeat(i-1);
+            String nombreEtoile = etoile.repeat(a);
+            System.out.println(nombreEspace + nombreEtoile + nombreEspace);
+            a = a + 2 ;
+        }
     }
 }
