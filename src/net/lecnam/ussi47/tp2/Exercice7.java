178eeaba5e2f481be07c116305fdd7bbef171c74
Maxime MONICHON--SIRONNEAU <maxime.monichonsironneau@gmail.com>
2026-09-10 11:24:51 +0200

    Exercice 7

--- src/net/lecnam/ussi47/tp2/Exercice7.java
+++ src/net/lecnam/ussi47/tp2/Exercice7.java
@@ -1,9 +1,18 @@
 package net.lecnam.ussi47.tp2;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Exercice7 {
 
     public static void main(String[] args) {
 
+        int aleatoire = ThreadLocalRandom.current().nextInt(0,101);
+
+        if (aleatoire % 2 == 1) {
+            System.out.println("Impair car " + aleatoire);
+        } else {
+            System.out.println("Pair car " + aleatoire);
+        }
 
     }
 }
