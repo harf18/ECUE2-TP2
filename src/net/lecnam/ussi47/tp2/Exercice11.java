6ae551026cf10dc3649fbca56610b9d3a3453493
Xavier Barbeau <xavierbarbeau41@gmail.com>
2026-09-10 14:30:27 +0200

    Correctif exercice 11

--- src/net/lecnam/ussi47/tp2/Exercice11.java
+++ src/net/lecnam/ussi47/tp2/Exercice11.java
@@ -5,5 +5,6 @@ public class Exercice11 {
     public static void main(String[] args) {
         String texteReceived = "CNAM";
         String texteModified = texteReceived.substring(0,1).toUpperCase() + texteReceived.substring(1).toLowerCase();
+        System.out.println("Texte reçu : " + texteReceived + "\nTexte modifier : " + texteModified);
     }
 }
