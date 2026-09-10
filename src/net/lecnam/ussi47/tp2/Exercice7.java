e39082d80f9913ef938a29bb237e1fdb7c9b6d57
Stephane Bance <bancestephane.r@gmail.com>
2026-09-10 08:31:13 -0200

    Exercice 7 Fini

--- src/net/lecnam/ussi47/tp2/Exercice7.java
+++ src/net/lecnam/ussi47/tp2/Exercice7.java
@@ -1,9 +1,16 @@
 package net.lecnam.ussi47.tp2;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Exercice7 {
 
     public static void main(String[] args) {
-
+        int val = ThreadLocalRandom.current().nextInt(1,101);
+        if (val % 2 == 0) {
+            System.out.println("le chiffre (" + val + ") est pair.");
+        } else {
+            System.out.println("le chiffre (" + val + ") est impair.");
+        }
 
     }
 }
