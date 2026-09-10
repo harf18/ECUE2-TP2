e57f4a6b1d5eb6ca6970490756cf9fd39ef1bc73
Mouchard Yannis <yannis.mouchard@gmail.com>
2026-09-10 11:32:31 +0200

    Exercice 10

--- src/net/lecnam/ussi47/tp2/Exercice10.java
+++ src/net/lecnam/ussi47/tp2/Exercice10.java
@@ -3,7 +3,9 @@ package net.lecnam.ussi47.tp2;
 public class Exercice10 {
 
     public static void main(String[] args) {
+        String hello = "HELLO WORLD";
 
-
+        System.out.println(hello.toLowerCase());
+        System.out.println(hello.toUpperCase());
     }
 }
