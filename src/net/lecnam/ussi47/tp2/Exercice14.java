a37d045989e1fec47ea1da7528de5603f5024fbf
Stephane Bance <bancestephane.r@gmail.com>
2026-09-10 11:39:35 -0200

    Exercice 14 Fini !

--- src/net/lecnam/ussi47/tp2/Exercice14.java
+++ src/net/lecnam/ussi47/tp2/Exercice14.java
@@ -3,6 +3,18 @@ package net.lecnam.ussi47.tp2;
 public class Exercice14 {
 
     public static void main(String[] args) {
+        String sentence = "Esope reste ici et se repose";
+        String sentenceTemp = sentence.toLowerCase().replaceAll("\\s+", "");
+        String sentenceTempReverse = "";
+
+        for (int i = sentenceTemp.length() - 1 ; i >= 0;i--)
+        {
+            sentenceTempReverse += sentenceTemp.charAt(i);
+        }
+
+        if (sentenceTemp.equals(sentenceTempReverse)) {
+            System.out.println("La phrase \"" + sentence + "\" est un palindrome.");
+        }
 
 
     }
