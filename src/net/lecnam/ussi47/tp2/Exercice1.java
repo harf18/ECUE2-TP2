35196ff41097624765fac5185337852e5b0d910f
Andrea Andreoli <andrea.andreoli.auditeur@lecnam.net>
2026-09-10 11:15:32 +0200

    Exercice 1

--- src/net/lecnam/ussi47/tp2/Exercice1.java
+++ src/net/lecnam/ussi47/tp2/Exercice1.java
@@ -3,7 +3,14 @@ package net.lecnam.ussi47.tp2;
 public class Exercice1 {
 
     public static void main(String[] args) {
-
-
+        byte age = 12 ;
+        float gramme = 2.26f ;
+        short euros = -300 ;
+        long pesos = 3000000000l ;
+        int yen = 2000000000 ;
+        char lettre = 'a' ;
+        String bonjourEnAnglais = "Hello" ;
+        byte nombreDeVoiture = 1 ;
+        short dette = 29000 ;
     }
 }
