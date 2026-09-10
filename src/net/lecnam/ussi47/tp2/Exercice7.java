84802491926d357a2eac4d1a431eca82c9d3240c
Zackarie Nouchet <zackarie.nouchet.auditeur@lecnam.net>
2026-09-10 11:51:54 +0200

    Exercice 7

--- src/net/lecnam/ussi47/tp2/Exercice7.java
+++ src/net/lecnam/ussi47/tp2/Exercice7.java
@@ -1,9 +1,16 @@
 package net.lecnam.ussi47.tp2;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Exercice7 {
 
     public static void main(String[] args) {
 
+        int nombre = ThreadLocalRandom.current().nextInt(1, 101);
 
+        if (nombre % 2 == 0)
+            System.out.println("Le nombre " + nombre + " est Pair");
+        else
+            System.out.println("Le nombre " + nombre + " est Impair");
     }
 }
