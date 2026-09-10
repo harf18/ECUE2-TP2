6dd1dc4841751d0e129a74751eb4f2a19a6484a3
Stephane Bance <bancestephane.r@gmail.com>
2026-09-10 10:58:50 -0200

    Exercice 10 Fini !

--- src/net/lecnam/ussi47/tp2/Exercice10.java
+++ src/net/lecnam/ussi47/tp2/Exercice10.java
@@ -3,7 +3,11 @@ package net.lecnam.ussi47.tp2;
 public class Exercice10 {
 
     public static void main(String[] args) {
+        String upperWord = "HELLO WORLD";
+        String lowerWord = "hello world";
 
+        System.out.println(upperWord.toLowerCase());
+        System.out.println(lowerWord.toUpperCase());
 
     }
 }
