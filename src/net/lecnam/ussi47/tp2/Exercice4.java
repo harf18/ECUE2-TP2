bc66487b766241ec4a3613958def05482ca3c1c6
Stephane Bance <bancestephane.r@gmail.com>
2026-09-10 07:56:17 -0200

    Exercice 4 Fini

--- src/net/lecnam/ussi47/tp2/Exercice4.java
+++ src/net/lecnam/ussi47/tp2/Exercice4.java
@@ -3,7 +3,10 @@ package net.lecnam.ussi47.tp2;
 public class Exercice4 {
 
     public static void main(String[] args) {
+        int meanFallDistance = 4000;
+        float coeffFeet = 3.28084f;
+        float meanFallDistanceInFeet = meanFallDistance * coeffFeet;
 
-
+        System.out.println(meanFallDistance + " x " + coeffFeet + " = " + meanFallDistanceInFeet);
     }
 }
