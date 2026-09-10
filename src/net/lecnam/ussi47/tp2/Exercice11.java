e700eaf27102b7bd56f0405b1443d3d17c86bd39
Xavier Barbeau <xavierbarbeau41@gmail.com>
2026-09-10 14:28:52 +0200

    Exercice 11

--- src/net/lecnam/ussi47/tp2/Exercice11.java
+++ src/net/lecnam/ussi47/tp2/Exercice11.java
@@ -3,7 +3,7 @@ package net.lecnam.ussi47.tp2;
 public class Exercice11 {
 
     public static void main(String[] args) {
-
-
+        String texteReceived = "CNAM";
+        String texteModified = texteReceived.substring(0,1).toUpperCase() + texteReceived.substring(1).toLowerCase();
     }
 }
