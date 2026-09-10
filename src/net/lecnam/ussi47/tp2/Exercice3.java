5ddd4131c9cf7b33ccafc0bfa8b470aab2e00941
Zackarie Nouchet <zackarie.nouchet.auditeur@lecnam.net>
2026-09-10 11:30:24 +0200

    Exercice 3

--- src/net/lecnam/ussi47/tp2/Exercice3.java
+++ src/net/lecnam/ussi47/tp2/Exercice3.java
@@ -4,6 +4,18 @@ public class Exercice3 {
 
     public static void main(String[] args) {
 
+        int nbJardin = 4;
+        float longueurLargeurJardin = 1.25f;
+        int nbPlantesParJardin =9;
+
+        float surfaceJardin = longueurLargeurJardin * longueurLargeurJardin;
+        int nbPlantes = nbJardin * nbPlantesParJardin;
+        float surfParPlante = surfaceJardin / nbPlantesParJardin;
+        float surfaceCultivee = surfaceJardin * nbJardin;
+
+        System.out.println("La surface totale cultivée est de " + surfaceCultivee + "m²");
+        System.out.println(nbPlantes + " plantes sont cultivables dans ce jardin");
+        System.out.println("Chaque plante a un espace de " + surfParPlante + " pour grandir");
 
     }
 }
