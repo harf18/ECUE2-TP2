774e1e8269e398fdfe429d4510d23432489d433f
Valentin Dujour <valentin.dujour@icloud.com>
2026-09-10 12:12:48 +0200

    Exercice 4 réalisé !

--- src/net/lecnam/ussi47/tp2/Exercice4.java
+++ src/net/lecnam/ussi47/tp2/Exercice4.java
@@ -3,7 +3,12 @@ package net.lecnam.ussi47.tp2;
 public class Exercice4 {
 
     public static void main(String[] args) {
+        int hauteurParachutiste = 4000;
+        double conversionPied = 3.28084;
 
+        double hauteurEnPied = hauteurParachutiste * conversionPied;
 
+        System.out.println("1 mètre correspond à " + conversionPied + " pieds.");
+        System.out.println("Le parachutiste saute de " + hauteurEnPied + " pieds. (4000m)");
     }
 }
