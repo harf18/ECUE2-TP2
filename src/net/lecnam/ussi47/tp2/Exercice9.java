d5126da9f8c9e6deef52be93d73a9748c6e67050
Alioune Diop <14diopalioune2003@gmail.com>
2026-09-10 16:26:26 +0200

    Exercice 9

--- src/net/lecnam/ussi47/tp2/Exercice9.java
+++ src/net/lecnam/ussi47/tp2/Exercice9.java
@@ -3,6 +3,15 @@ package net.lecnam.ussi47.tp2;
 public class Exercice9 {
 
     public static void main(String[] args) {
+        String chaine = "3896,86 / 4,869 = ";
+        String nombre1 = chaine.substring(0,7);
+        String nombre2 = chaine.substring(10,15);
+        System.out.println(nombre2);
+        float transtyp_nombre1 = Float.parseFloat(nombre1.replace(",","."));
+        float transtyp_nombre2 = Float.parseFloat(nombre2.replace(",","."));
+        System.out.println(transtyp_nombre1);
+        float resultat = transtyp_nombre1/transtyp_nombre2;
+        System.out.println(chaine + resultat);
 
 
     }
