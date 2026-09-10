511608f922ebd69988b0a502781697253a1b6dc0
Alioune Diop <14diopalioune2003@gmail.com>
2026-09-10 12:03:49 +0200

    Exercice 2

--- src/net/lecnam/ussi47/tp2/Exercice2.java
+++ src/net/lecnam/ussi47/tp2/Exercice2.java
@@ -3,7 +3,15 @@ package net.lecnam.ussi47.tp2;
 public class Exercice2 {
 
     public static void main(String[] args) {
-
-
+        byte a = 3;
+        byte b = 4;
+        byte c = 5;
+        double resultat , hypo;
+        resultat = Math.pow(a,2) + Math.pow(b,2);
+        hypo = Math.pow(c,2);
+        if (resultat == hypo)
+            System.out.println("le théoréme est vérifier");
+        else
+            System.out.println("le théoréme n'est pas réel");
     }
 }
