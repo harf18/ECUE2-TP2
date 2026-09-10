6d836841a59522bf0bafa1dcfdcafddfa4716f36
Mouchard Yannis <yannis.mouchard@gmail.com>
2026-09-10 14:02:49 +0200

    Exercice 17

--- src/net/lecnam/ussi47/tp2/Exercice15.java
+++ src/net/lecnam/ussi47/tp2/Exercice15.java
@@ -5,6 +5,7 @@ public class Exercice15 {
 
     public static void main(String[] args) {
         int x = 5;
+
         for (int i = 0; i < x; i++) {
             for (int j = 0; j < x - i - 1; j++) {
                 System.out.print(" ");
