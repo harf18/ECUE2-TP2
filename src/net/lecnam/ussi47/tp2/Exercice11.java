dafb3b86bfa6cb5a9d3a201bbd19a9f45f7a6082
Valentin Dujour <valentin.dujour@icloud.com>
2026-09-10 16:11:05 +0200

    Exercice 11 réalisé !

--- src/net/lecnam/ussi47/tp2/Exercice11.java
+++ src/net/lecnam/ussi47/tp2/Exercice11.java
@@ -1,9 +1,15 @@
 package net.lecnam.ussi47.tp2;
 
+import java.util.Locale;
+
 public class Exercice11 {
 
     public static void main(String[] args) {
+        String texte = "DGAC";
+        char premiereLettre = texte.charAt(0);
+        String finDuMot = texte.substring(1, texte.length()).toLowerCase();
 
+        System.out.println(premiereLettre + finDuMot);
 
     }
 }
