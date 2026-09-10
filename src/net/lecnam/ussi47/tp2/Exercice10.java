55046c577895819d9f11cd748de648f3b4426a09
Zackarie Nouchet <zackarie.nouchet.auditeur@lecnam.net>
2026-09-10 12:15:38 +0200

    Exercice 10

--- src/net/lecnam/ussi47/tp2/Exercice10.java
+++ src/net/lecnam/ussi47/tp2/Exercice10.java
@@ -4,6 +4,10 @@ public class Exercice10 {
 
     public static void main(String[] args) {
 
+        String chaine1 = "HELLO WORLD";
+        String chaine2 = "hello world";
 
+        System.out.println(chaine1.toLowerCase());
+        System.out.println(chaine2.toUpperCase());
     }
 }
