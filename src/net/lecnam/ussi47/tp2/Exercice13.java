ee875d11f12d58b31d6c36aa0fa8112fdcbab16d
Mouchard Yannis <yannis.mouchard@gmail.com>
2026-09-10 11:58:19 +0200

    Exercice 13

--- src/net/lecnam/ussi47/tp2/Exercice13.java
+++ src/net/lecnam/ussi47/tp2/Exercice13.java
@@ -1,9 +1,16 @@
 package net.lecnam.ussi47.tp2;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Exercice13 {
 
     public static void main(String[] args) {
+        int facto = ThreadLocalRandom.current().nextInt(0,9);
+        double resultat = 1;
 
-
+        for (int i = 1; i <= facto; i++){
+            resultat = resultat*i;
+        }
+        System.out.println(facto + " factorielle = " +resultat);
     }
 }
