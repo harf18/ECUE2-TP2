957163ed3cfd4170fbc6491146709023be5df87e
Zackarie Nouchet <zackarie.nouchet.auditeur@lecnam.net>
2026-09-10 12:18:20 +0200

    Exercice 11

--- src/net/lecnam/ussi47/tp2/Exercice11.java
+++ src/net/lecnam/ussi47/tp2/Exercice11.java
@@ -4,6 +4,10 @@ public class Exercice11 {
 
     public static void main(String[] args) {
 
+        String chaine = "CNAM";
 
+        String resultat = chaine.substring(0, 1) + chaine.substring(1).toLowerCase();
+
+        System.out.println(resultat);
     }
 }
