60c34a2824921ce52d329ff691e3c7e442cf59c5
Rémi Pierron <remipierron@MacBook-Air-de-Remi.local>
2026-09-10 11:33:51 +0200

    exercice 2

--- src/net/lecnam/ussi47/tp2/Exercice2.java
+++ src/net/lecnam/ussi47/tp2/Exercice2.java
@@ -4,6 +4,26 @@ public class Exercice2 {
 
     public static void main(String[] args) {
 
+        // démontrer le theoreme de pythagore
+
+        byte a = 3;
+        byte b = 4;
+        byte c = 5;
+
+        int carreA;
+        int carreB;
+        int carreC;
+
+        int sommeCote;
+
+        carreA = a * a;
+        carreB = b * b;
+        carreC = c * c;
+
+        sommeCote = carreA + carreB;
+
+        if(sommeCote == carreC)
+            System.out.println("theoreme valide.");
 
     }
 }
