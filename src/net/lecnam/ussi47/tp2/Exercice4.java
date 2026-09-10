758300bcb85a21f6b35dd207eef66d59a5eb0ed6
Maxime MONICHON--SIRONNEAU <maxime.monichonsironneau@gmail.com>
2026-09-10 11:17:01 +0200

    Exercice 4

--- src/net/lecnam/ussi47/tp2/Exercice4.java
+++ src/net/lecnam/ussi47/tp2/Exercice4.java
@@ -4,6 +4,11 @@ public class Exercice4 {
 
     public static void main(String[] args) {
 
+        int hauteur = 4000;
+        double piedEnM = 0.3;
+
+        System.out.println("La conversion de 1 pied en mètre est de 1 pied pour 30cm.");
+        System.out.println("La hauteur en pied est de : " + hauteur/piedEnM);
 
     }
 }
