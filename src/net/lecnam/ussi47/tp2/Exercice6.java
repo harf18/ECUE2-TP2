f031b6f467e5cf97058054f884a153110f9211aa
Xavier Barbeau <xavierbarbeau41@gmail.com>
2026-09-10 12:23:57 +0200

    Exercice 6

--- src/net/lecnam/ussi47/tp2/Exercice6.java
+++ src/net/lecnam/ussi47/tp2/Exercice6.java
@@ -1,9 +1,15 @@
 package net.lecnam.ussi47.tp2;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Exercice6 {
 
     public static void main(String[] args) {
-
-
+        byte randomNumber= (byte) ThreadLocalRandom.current().nextInt(0,2);
+        if (randomNumber == 0) {
+            System.out.println(false);
+        } else if (randomNumber == 1) {
+            System.out.println(true);
+        }
     }
 }
