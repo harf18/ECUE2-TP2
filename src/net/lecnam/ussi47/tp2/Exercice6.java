d3f6fa734e84e7e328bfd62a0bf56c8a4f035770
Andrea Andreoli <andrea.andreoli.auditeur@lecnam.net>
2026-09-10 12:16:46 +0200

    Exercice 6

--- src/net/lecnam/ussi47/tp2/Exercice6.java
+++ src/net/lecnam/ussi47/tp2/Exercice6.java
@@ -1,9 +1,16 @@
 package net.lecnam.ussi47.tp2;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Exercice6 {
 
     public static void main(String[] args) {
-
-
+        if (ThreadLocalRandom.current().nextInt(0,2) == 1) {
+            boolean resultat = true ;
+            System.out.println(resultat);
+        } else {
+            boolean resultat = false ;
+            System.out.println(resultat);
+        }
     }
 }
