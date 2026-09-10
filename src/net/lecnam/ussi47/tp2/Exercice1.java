07d5d583181b99e7b6bbb056afe646a1a263c16b
Maxime MONICHON--SIRONNEAU <maxime.monichonsironneau@gmail.com>
2026-09-10 10:55:52 +0200

    Exercice 1

--- src/net/lecnam/ussi47/tp2/Exercice1.java
+++ src/net/lecnam/ussi47/tp2/Exercice1.java
@@ -4,6 +4,15 @@ public class Exercice1 {
 
     public static void main(String[] args) {
 
+        int nombre1 = 12;
+        double nombre2 = 2.26;
+        int nombre3 = -300;
+        long nombre4 = 3000000000L;
+        long nombre5 = 2000000000L;
+        String chaine1 = "a";
+        String chaine2 = "Hello";
+        byte nombre6 = 1;
+        int nombre7 = 29000;
 
     }
 }
