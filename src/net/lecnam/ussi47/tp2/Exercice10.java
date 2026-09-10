6f4756bd087688f80859a78043d1877d82c8af5b
Xavier Barbeau <xavierbarbeau41@gmail.com>
2026-09-10 14:22:37 +0200

    Exercice 10

--- src/net/lecnam/ussi47/tp2/Exercice10.java
+++ src/net/lecnam/ussi47/tp2/Exercice10.java
@@ -3,7 +3,9 @@ package net.lecnam.ussi47.tp2;
 public class Exercice10 {
 
     public static void main(String[] args) {
-
-
+        String helloWorldVar = "HELLO WORLD";
+        String helloWorldLowCase = helloWorldVar.toLowerCase();
+        String helloWorldUpCase = helloWorldLowCase.toUpperCase();
+        System.out.println("Texte reçu : " + helloWorldVar + "\nPasser en minuscule : " + helloWorldLowCase + "\nPasser en majuscule à nouveau : " + helloWorldUpCase);
     }
 }
