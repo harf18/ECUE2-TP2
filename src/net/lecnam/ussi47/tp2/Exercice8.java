7486c124b5fbd90bcb5f72e4ee0c18e759d03aee
Maxime MONICHON--SIRONNEAU <maxime.monichonsironneau@gmail.com>
2026-09-10 11:28:11 +0200

    Exercice 8

--- src/net/lecnam/ussi47/tp2/Exercice8.java
+++ src/net/lecnam/ussi47/tp2/Exercice8.java
@@ -1,9 +1,22 @@
 package net.lecnam.ussi47.tp2;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Exercice8 {
 
     public static void main(String[] args) {
 
+        int annee = ThreadLocalRandom.current().nextInt(1900,2050);
+
+        if (annee % 4 != 0) {
+            System.out.println("Non bissextile car " + annee);
+        } else if (annee % 100 != 0) {
+            System.out.println("Bissextile car " + annee);
+        } else if (annee % 400 != 0) {
+            System.out.println("Non bissextile car " + annee);
+        } else {
+            System.out.println("Bissextile car " + annee);
+        }
 
     }
 }
