5d260f2e2ec6772382d246cc3d529b7322801fee
Stephane Bance <bancestephane.r@gmail.com>
2026-09-10 07:47:26 -0200

    cammelCase fix

--- src/net/lecnam/ussi47/tp2/Exercice2.java
+++ src/net/lecnam/ussi47/tp2/Exercice2.java
@@ -5,11 +5,11 @@ import static java.lang.Math.pow;
 public class Exercice2 {
 
     public static void main(String[] args) {
-        int c_adjacent = 3;
-        int c_oppose = 4;
+        int cAdjacent = 3;
+        int cOppose = 4;
         int hypotenuse = 5;
 
-        boolean test_pytagore = pow(c_adjacent, 2) + pow(c_oppose, 2) == pow(hypotenuse, 2);
+        boolean test_pytagore = pow(cAdjacent, 2) + pow(cOppose, 2) == pow(hypotenuse, 2);
 
         if (test_pytagore) System.out.println("Le triangle est rectangle.");
 
--- src/net/lecnam/ussi47/tp2/Exercice3.java
+++ src/net/lecnam/ussi47/tp2/Exercice3.java
@@ -3,20 +3,20 @@ package net.lecnam.ussi47.tp2;
 public class Exercice3 {
 
     public static void main(String[] args) {
-        int n_square = 4;
-        int plant_by_square = 9;
-        float side_square_length = 1.25f;
-        float surface = side_square_length * side_square_length;
-        float side_plant_length = side_square_length/plant_by_square;
+        int nSquare = 4;
+        int plantBySquare = 9;
+        float sideSquareLength = 1.25f;
+        float surface = sideSquareLength * sideSquareLength;
+        float sidePlantLength = sideSquareLength/plantBySquare;
 
-        float total_surfaces = 4 * surface;
-        int total_plants = n_square * plant_by_square;
-        float plant_surface = side_plant_length * side_plant_length;
+        float totalSurfaces = 4 * surface;
+        int totalPlants = nSquare * plantBySquare;
+        float plantSurface = sidePlantLength * sidePlantLength;
 
 
-        System.out.println("Total surface : " + total_surfaces);
-        System.out.println("Total plantes : " + total_plants);
-        System.out.println("Plante surface : " + plant_surface);
+        System.out.println("Total surface : " + totalSurfaces);
+        System.out.println("Total plantes : " + totalPlants);
+        System.out.println("Plante surface : " + plantSurface);
 
 
     }
