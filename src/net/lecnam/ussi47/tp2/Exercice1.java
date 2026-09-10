c0ca4c7995a0104d2bbf83875364e4ec3c1901b9
Valentin Dujour <valentin.dujour@icloud.com>
2026-09-10 11:12:17 +0200

    Exercie 1 réalisé !

--- src/net/lecnam/ussi47/tp2/Exercice1.java
+++ src/net/lecnam/ussi47/tp2/Exercice1.java
@@ -3,7 +3,14 @@ package net.lecnam.ussi47.tp2;
 public class Exercice1 {
 
     public static void main(String[] args) {
-
+        byte value1 = 12;
+        double value2 = 2.26;
+        short value3 = -300;
+        long value4 = 3000000000L;
+        long value5 = 2000000000L;
+        String value6 = "a";
+        String value7 = "Hello";
+        char value8 = 29000;
 
     }
 }
