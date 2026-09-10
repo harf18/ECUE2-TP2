19539576836592abcf5877cbdc1acacf7d55e917
Xavier Barbeau <xavierbarbeau41@gmail.com>
2026-09-10 14:11:06 +0200

    Exercice 8

--- src/net/lecnam/ussi47/tp2/Exercice8.java
+++ src/net/lecnam/ussi47/tp2/Exercice8.java
@@ -1,9 +1,15 @@
 package net.lecnam.ussi47.tp2;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Exercice8 {
 
     public static void main(String[] args) {
-
-
+        short randomYear = (short) ThreadLocalRandom.current().nextInt(1900,2050);
+        if (((randomYear % 4 == 0) && (randomYear % 100 != 0)) || (randomYear % 400 == 0)) {
+            System.out.println("L'année : " + randomYear + " est bissextile");
+        } else {
+            System.out.println("L'année : " + randomYear + " n'est pas bissextile");
+        }
     }
 }
