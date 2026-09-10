62c4d2dc6bea96ab4983e8fdad4adbf9353faa4e
Mouchard Yannis <yannis.mouchard@gmail.com>
2026-09-10 11:35:45 +0200

    Exercice 11

--- src/net/lecnam/ussi47/tp2/Exercice11.java
+++ src/net/lecnam/ussi47/tp2/Exercice11.java
@@ -3,7 +3,8 @@ package net.lecnam.ussi47.tp2;
 public class Exercice11 {
 
     public static void main(String[] args) {
+        String cnam = "CNAM";
 
-
+        System.out.println(cnam.substring(0,1).toUpperCase() + cnam.substring(1).toLowerCase());
     }
 }
