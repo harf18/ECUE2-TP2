ee5dcc336f53c25662df0a990ba3fe616d6efab3
Stephane Bance <bancestephane.r@gmail.com>
2026-09-10 10:40:44 -0200

    Exercice 9 Fini

--- src/net/lecnam/ussi47/tp2/Exercice9.java
+++ src/net/lecnam/ussi47/tp2/Exercice9.java
@@ -4,6 +4,13 @@ public class Exercice9 {
 
     public static void main(String[] args) {
 
+        float val1 = 3896.86f;
+        float val2 = 4.869f;
+        float result = val1/val2;
+
+        System.out.println(val1 + " / " + val2 + " = " + result);
+
+
 
     }
 }
