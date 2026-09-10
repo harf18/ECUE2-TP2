23f967a2590b30567fd8e82f7b944ed935a49a25
Julien Forestier <julien.forestier.auditeur@lecnam.net>
2026-09-10 11:29:16 +0200

    Exercice3

--- src/net/lecnam/ussi47/tp2/Exercice3.java
+++ src/net/lecnam/ussi47/tp2/Exercice3.java
@@ -3,7 +3,16 @@ package net.lecnam.ussi47.tp2;
 public class Exercice3 {
 
     public static void main(String[] args) {
+        int nbCarre = 4;
+        float surfaceCarre = 1.25f * 1.25f;
+        int nbPlantesParCarre = 9;
 
+        float surfaceTotale = nbCarre * surfaceCarre;
+        float nbPlantesTotales = nbCarre * nbPlantesParCarre;
+        float surfaceParPlantes = surfaceTotale / nbPlantesTotales;
 
+        System.out.println(surfaceTotale);
+        System.out.println(nbPlantesTotales);
+        System.out.println(surfaceParPlantes);
     }
 }
