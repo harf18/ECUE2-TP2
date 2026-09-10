e8aba886fb5d89da73d636aacd2b5e18b75cb582
Stephane Bance <bancestephane.r@gmail.com>
2026-09-10 10:38:10 -0200

    Exercice 8 Fini

--- src/net/lecnam/ussi47/tp2/Exercice8.java
+++ src/net/lecnam/ussi47/tp2/Exercice8.java
@@ -1,9 +1,17 @@
 package net.lecnam.ussi47.tp2;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Exercice8 {
 
     public static void main(String[] args) {
+        int randYear = ThreadLocalRandom.current().nextInt(1900,2050);
 
-
+        // Fist way
+        if (((randYear % 4 == 0) && (randYear % 100 != 0)) || (randYear % 400 == 0)) {
+            System.out.println(randYear + " est une année bissextile.");
+        } else {
+            System.out.println(randYear + " n'est pas une année bissextile");
+        }
     }
 }
