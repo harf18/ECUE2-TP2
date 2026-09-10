54765697539deb997c82355ab7ae48b7c37c3b71
Stephane Bance <bancestephane.r@gmail.com>
2026-09-10 08:28:19 -0200

    Exercice 6 Fini

--- src/net/lecnam/ussi47/tp2/Exercice6.java
+++ src/net/lecnam/ussi47/tp2/Exercice6.java
@@ -1,9 +1,15 @@
 package net.lecnam.ussi47.tp2;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Exercice6 {
 
     public static void main(String[] args) {
-
+        if (ThreadLocalRandom.current().nextInt(0,2) == 1) {
+            System.out.println("True");
+        } else {
+            System.out.println("False");
+        }
 
     }
 }
