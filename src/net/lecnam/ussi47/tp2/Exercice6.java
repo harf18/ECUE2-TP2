aebe9211d262a576b9a8535de9ff06f8f5f6a1dc
Maxime MONICHON--SIRONNEAU <maxime.monichonsironneau@gmail.com>
2026-09-10 11:23:15 +0200

    Exercice 6

--- src/net/lecnam/ussi47/tp2/Exercice6.java
+++ src/net/lecnam/ussi47/tp2/Exercice6.java
@@ -1,9 +1,18 @@
 package net.lecnam.ussi47.tp2;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Exercice6 {
 
     public static void main(String[] args) {
 
+        int aleatoire = ThreadLocalRandom.current().nextInt(0,2);
+
+        if (aleatoire == 1) {
+            System.out.println("True");
+        } else {
+            System.out.println("False");
+        }
 
     }
 }
