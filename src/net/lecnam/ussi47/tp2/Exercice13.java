e2599c77e1f6e78379446729f592150f652b8f39
Valentin Dujour <valentin.dujour@icloud.com>
2026-09-10 16:51:11 +0200

    Exercice 13 réalisé !

--- src/net/lecnam/ussi47/tp2/Exercice13.java
+++ src/net/lecnam/ussi47/tp2/Exercice13.java
@@ -1,9 +1,20 @@
 package net.lecnam.ussi47.tp2;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Exercice13 {
 
     public static void main(String[] args) {
+        int nombre = ThreadLocalRandom.current().nextInt(0,9);
+        int factorielle = 1;
 
+        System.out.println("Le nombre est " + nombre);
+
+        for (int i = 1 ; i <= nombre ; i++) {
+            factorielle *= i;
+        }
+
+        System.out.println("Le factorielle de " + nombre + " est " + factorielle);
 
     }
 }
