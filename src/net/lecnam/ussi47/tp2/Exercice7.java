9da710cd5812b24aa36c61117af4b3d15516d159
Julien Forestier <julien.forestier.auditeur@lecnam.net>
2026-09-10 11:53:09 +0200

    Exercice7

--- src/net/lecnam/ussi47/tp2/Exercice7.java
+++ src/net/lecnam/ussi47/tp2/Exercice7.java
@@ -1,9 +1,17 @@
 package net.lecnam.ussi47.tp2;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Exercice7 {
 
     public static void main(String[] args) {
-
-
+        int nombre = ThreadLocalRandom.current().nextInt(1,101 );
+        System.out.println(nombre);
+        if (nombre %2 == 1) {
+            System.out.println("Impair");
+        }
+        if (nombre %2 == 0) {
+            System.out.println("Pair");
+        }
     }
 }
