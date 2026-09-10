1827e61691d854c1b8eb0fef74c1ecd8a27a625e
Maxime MONICHON--SIRONNEAU <maxime.monichonsironneau@gmail.com>
2026-09-10 12:18:48 +0200

    Exercice 15

--- src/net/lecnam/ussi47/tp2/Exercice15.java
+++ src/net/lecnam/ussi47/tp2/Exercice15.java
@@ -4,6 +4,12 @@ public class Exercice15 {
 
     public static void main(String[] args) {
 
+        int tailleSapin = 11;
+        int correctionTailleSapin = tailleSapin + ((tailleSapin-1) % 2);
+
+        for (int i = 1; i < correctionTailleSapin; i += 2) {
+            System.out.println(" ".repeat((correctionTailleSapin - 1)/2 - i/2) + "*".repeat(i) + " ".repeat((correctionTailleSapin - 1)/2 - i/2));
+        }
 
     }
 }
