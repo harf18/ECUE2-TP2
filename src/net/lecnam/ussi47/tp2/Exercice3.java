ad84e14bd098b65a7378cce2b66ac9c444a7458a
Rémi Pierron <remipierron@MacBook-Air-de-Remi.local>
2026-09-10 11:51:55 +0200

    exo3

--- src/net/lecnam/ussi47/tp2/Exercice3.java
+++ src/net/lecnam/ussi47/tp2/Exercice3.java
@@ -4,6 +4,26 @@ public class Exercice3 {
 
     public static void main(String[] args) {
 
+        //Un jardinier amateur veut créer 4 carrés jardin de 1,25m x 1,25m.
+        // Il peut planter 9 plantes par carré.
+        // Ecrire un programme qui affiche la surface cultivée, le nombre plantes cultivables
+        // et la surface par plante.
+
+
+        // valeurs de base
+        byte nbJardins = 4;
+        float coteJardins = 1.25f;
+        byte nbPlantesParJardins = 9;
+
+        // valeurs calculées
+        double surfaceJardins = Math.pow(coteJardins, 2);
+        double surfaceCultivee = nbJardins * surfaceJardins;
+        int nbPlantesCaltivables = nbJardins * nbPlantesParJardins;
+        double surfaceParPlante = surfaceCultivee / nbPlantesCaltivables;
+
+        System.out.println("surface cultivee : " + surfaceCultivee);
+        System.out.println("nombre de plantes cultivables : " + nbPlantesCaltivables);
+        System.out.println("surface par plante : " + surfaceParPlante);
 
     }
 }
