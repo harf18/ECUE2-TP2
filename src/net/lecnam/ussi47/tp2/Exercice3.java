de786c89a948edf144d3807bef604c4539c00a97
Andrea Andreoli <andrea.andreoli.auditeur@lecnam.net>
2026-09-10 11:47:21 +0200

    Exercice 3

--- src/net/lecnam/ussi47/tp2/Exercice3.java
+++ src/net/lecnam/ussi47/tp2/Exercice3.java
@@ -3,7 +3,11 @@ package net.lecnam.ussi47.tp2;
 public class Exercice3 {
 
     public static void main(String[] args) {
-
+        float surfaceCarreJardin = 1.25f * 1.25f ;
+        float surfaceCultivable = 4 * surfaceCarreJardin ;
+        byte nombreDePlanteCultivable = 9 * 4 ;
+        float surfaceParPlante = surfaceCarreJardin / 9 ;
+        System.out.println("La surface cultivée est de " + surfaceCultivable + ", le nombre plantes cultivables est de " + nombreDePlanteCultivable + " et la surface par plante est de " + surfaceParPlante + ".");
 
     }
 }
