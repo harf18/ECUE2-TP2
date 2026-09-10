a6bcc76cad3a4a34236346a208b6cacf048aa22b
Stephane Bance <bancestephane.r@gmail.com>
2026-09-10 10:55:57 -0200

    Exercice 11 Fini !

--- src/net/lecnam/ussi47/tp2/Exercice11.java
+++ src/net/lecnam/ussi47/tp2/Exercice11.java
@@ -3,7 +3,12 @@ package net.lecnam.ussi47.tp2;
 public class Exercice11 {
 
     public static void main(String[] args) {
+        String word = "CNAM";
+        String firstLetter = word.substring(0, 1);
+        String restWord = word.substring(1);
 
+        String capitalizeWord = firstLetter.toUpperCase() + restWord.toLowerCase();
 
+        System.out.println(capitalizeWord);
     }
 }
