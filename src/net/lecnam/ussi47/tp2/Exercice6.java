e624390cdc8e64709deca5ee5f20f67bd5ac2559
Valentin Dujour <valentin.dujour@icloud.com>
2026-09-10 15:03:41 +0200

    Exercice 6 réalisé !

--- src/net/lecnam/ussi47/tp2/Exercice6.java
+++ src/net/lecnam/ussi47/tp2/Exercice6.java
@@ -1,9 +1,18 @@
 package net.lecnam.ussi47.tp2;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Exercice6 {
 
     public static void main(String[] args) {
+        int trueOrFalse = ThreadLocalRandom.current().nextInt(0,2);
 
+        System.out.println("Le résultat est : " + trueOrFalse);
+        if (trueOrFalse == 1) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
 
     }
 }
