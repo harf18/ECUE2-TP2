9a6a44f3736c8377cabdb4c095e725245ec80cd9
Rémi Pierron <remipierron@MacBook-Air-de-Remi.local>
2026-09-11 09:27:11 +0200

    exo6

--- src/net/lecnam/ussi47/tp2/Exercice6.java
+++ src/net/lecnam/ussi47/tp2/Exercice6.java
@@ -1,9 +1,21 @@
 package net.lecnam.ussi47.tp2;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Exercice6 {
 
     public static void main(String[] args) {
 
+        int test = ThreadLocalRandom.current().nextInt(0,2);
+        boolean unOuZero;
 
+
+        if (test == 0)
+            unOuZero = false;
+        else
+            unOuZero = true;
+
+
+        System.out.println(unOuZero);
     }
 }
