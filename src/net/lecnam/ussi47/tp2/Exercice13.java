e8dd743e482f0b889732ebb45ade1bbd1bc211b1
Rémi Pierron <remipierron@MacBook-Air-de-Remi.local>
2026-09-11 14:56:20 +0200

    exo13

--- src/net/lecnam/ussi47/tp2/Exercice13.java
+++ src/net/lecnam/ussi47/tp2/Exercice13.java
@@ -1,9 +1,22 @@
 package net.lecnam.ussi47.tp2;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Exercice13 {
 
     public static void main(String[] args) {
 
+        int nombre = ThreadLocalRandom.current().nextInt(0,9);
+        int i = 1;
+        int calcul = 1;
 
+        while (true) {
+            calcul = calcul * nombre;
+            nombre--;
+            if (nombre == 0) {
+                break;
+            }
+        }
+        System.out.println(calcul);
     }
 }
