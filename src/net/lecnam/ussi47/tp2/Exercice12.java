17e0950939d19ab7f56ddb2ea23765d3a153b99a
Rémi Pierron <remipierron@MacBook-Air-de-Remi.local>
2026-09-11 14:26:46 +0200

    exo12

--- src/net/lecnam/ussi47/tp2/Exercice12.java
+++ src/net/lecnam/ussi47/tp2/Exercice12.java
@@ -1,9 +1,36 @@
 package net.lecnam.ussi47.tp2;
 
+import java.util.*;
+
 public class Exercice12 {
 
     public static void main(String[] args) {
 
+        String phrase = "cette phrase contient 11 voyelles";
+
+        // Liste des voyelles
+        List<Character> voyelles = Arrays.asList('a', 'e', 'i', 'o', 'u', 'y');
+
+        // Liste des consonnes
+        List<Character> consonnes = Arrays.asList('b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'z');
+
+        int i = 0;
+
+        int nbVoyelles = 0;
+
+        while (i < phrase.length()) {
+            char lettre = phrase.charAt(i);
+
+            if (voyelles.contains(lettre)) {
+                nbVoyelles++;
+            }
+
+
+
+            //System.out.println(lettre);
+            i++;
+        }
+        System.out.println("nombre de voyelles : " + nbVoyelles);
 
     }
 }
