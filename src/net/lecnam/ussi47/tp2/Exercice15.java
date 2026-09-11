bcb6e707902c72fe5043b339902e495f3100ab6e
Rémi Pierron <remipierron@MacBook-Air-de-Remi.local>
2026-09-11 16:26:08 +0200

    exo15

--- src/net/lecnam/ussi47/tp2/Exercice15.java
+++ src/net/lecnam/ussi47/tp2/Exercice15.java
@@ -1,9 +1,24 @@
 package net.lecnam.ussi47.tp2;
 
+import java.util.Scanner;
+
 public class Exercice15 {
 
     public static void main(String[] args) {
 
+        int x = 5;
+
+        for (int i = 1; i <= x; i++) {
+            // Espaces avant les étoiles
+            for (int j = 0; j < x - i; j++) {
+                System.out.print(" ");
+            }
+            // Étoiles
+            for (int k = 0; k < 2 * i - 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
 
     }
 }
