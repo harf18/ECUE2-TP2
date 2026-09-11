565f22de35d9e2933aae2bb2c0da551390395478
Rémi Pierron <remipierron@MacBook-Air-de-Remi.local>
2026-09-11 15:47:42 +0200

    exo14

--- src/net/lecnam/ussi47/tp2/Exercice14.java
+++ src/net/lecnam/ussi47/tp2/Exercice14.java
@@ -4,6 +4,32 @@ public class Exercice14 {
 
     public static void main(String[] args) {
 
+        String phrase = "Esope reste ici et se repose";
+        String phraseLower = phrase.toLowerCase();
+        String phraseSansEspaces = phraseLower.replace(" ", "");
+
+        int i = 0;
+        int j = (phraseSansEspaces.length() - 1);
+        boolean palindrome = true;
+
+        while(true){
+            char a = phraseSansEspaces.charAt(i);
+            char b = phraseSansEspaces.charAt(j);
+            if (a!=b) {
+
+                palindrome = false;
+                break;
+            }
+            else if (i == (phraseSansEspaces.length() - 1)) {
+                break;
+            }
+            i++;
+            j--;
+        }
+        if (palindrome == false)
+            System.out.println("La phrase n'est pas un palimdrome.");
+        else
+            System.out.println("La phrase est un palimdrome.");
 
     }
 }
