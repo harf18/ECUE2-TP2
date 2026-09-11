c86a4c3eea5b7df0088238669f5724c4c5516be8
Rémi Pierron <remipierron@MacBook-Air-de-Remi.local>
2026-09-11 12:07:12 +0200

    exo9

--- src/net/lecnam/ussi47/tp2/Exercice9.java
+++ src/net/lecnam/ussi47/tp2/Exercice9.java
@@ -4,6 +4,14 @@ public class Exercice9 {
 
     public static void main(String[] args) {
 
+        float premierChiffre = 3896.86f;
+        float secondChiffre = 4.869f;
+
+        float resultat = premierChiffre / secondChiffre;
+
+        String chaine = premierChiffre + " / " + secondChiffre + " = ";
+
+        System.out.println(chaine + resultat);
 
     }
 }
