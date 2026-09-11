49d85b0b5c5807a70354654c3c6cfd310d634685
Rémi Pierron <remipierron@MacBook-Air-de-Remi.local>
2026-09-11 09:41:21 +0200

    exo7

--- src/net/lecnam/ussi47/tp2/Exercice7.java
+++ src/net/lecnam/ussi47/tp2/Exercice7.java
@@ -1,9 +1,18 @@
 package net.lecnam.ussi47.tp2;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Exercice7 {
 
     public static void main(String[] args) {
 
+        int test = ThreadLocalRandom.current().nextInt(1,101);
+
+        if (test % 2 == 0) {
+            System.out.println("Le chiffre " + test + " est pair.");
+        }
+        else
+            System.out.println("Le chiffre " + test + " est impair.");
 
     }
 }
