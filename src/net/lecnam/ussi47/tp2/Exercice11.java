7cc6e80afb7b8814e52a326360a0f4df921d63eb
Rémi Pierron <remipierron@MacBook-Air-de-Remi.local>
2026-09-11 12:27:47 +0200

    exo11

--- src/net/lecnam/ussi47/tp2/Exercice11.java
+++ src/net/lecnam/ussi47/tp2/Exercice11.java
@@ -4,6 +4,15 @@ public class Exercice11 {
 
     public static void main(String[] args) {
 
+        String mot = "CNAM";
+        char premiereLettre = mot.charAt(0);
+        String autresLettres = mot.substring(1);
+        String autresLettresMin = autresLettres.toLowerCase();
+
+        String motFinal = premiereLettre + autresLettresMin;
+
+        System.out.println(motFinal);
+
 
     }
 }
